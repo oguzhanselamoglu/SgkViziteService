4eef4e4d2fb0f4883b1ce7146434a27e79d15aa0
oguzhanselamoglu <oguzhanselamoglu@outlook.com>
2023-03-09 15:22:22 +0300

    Add project files.

---
+++ SgkViziteService/Program.cs
@@ -0,0 +1,75 @@
+﻿using SgkViziteService.Services;
+using System.Security.Policy;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Configuration.GetSection("AzureConnectionStrings")["CloudConStr"];
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+
+app.MapGet("/login", async () =>
+{
+
+    SgkService service = new SgkService("https://uyg.sgk.gov.tr/Ws_Vizite/services/ViziteGonder", "", "", "");
+    var response = await service.LogiAsyncn();
+    return response;
+}).WithName("login").WithOpenApi();
+
+
+app.MapGet("/isyeriGoruntu", async () =>
+{
+
+    SgkService service = new SgkService("https://uyg.sgk.gov.tr/Ws_Vizite/services/ViziteGonder", "", "", "");
+
+    var response = await service.IsverenIletisimBilgileriGoruntuAsync();
+    return response;
+}).WithName("isyeriGoruntu").WithOpenApi();
+app.MapGet("/Raporarama", async (string tckNo) =>
+{
+
+    SgkService service = new SgkService("https://uyg.sgk.gov.tr/Ws_Vizite/services/ViziteGonder", "", "", "");
+
+    var response = await service.RaporAramaKimlikNoAsync(tckNo);
+    return response;
+}).WithName("Raporarama").WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ SgkViziteService/Services/SgkService.cs
@@ -0,0 +1,109 @@
+﻿using SgkViziteService.Models;
+using System.ServiceModel;
+
+namespace SgkViziteService.Services
+{
+    public class SgkService
+    {
+
+        public EndpointAddress endpointAddress;
+        public BasicHttpBinding basicHttpBinding;
+        private readonly ViziteGonderService.ViziteGonderClient _service;
+        private string Token { get; set; }
+        public string IsyeriKodu { get; set; }
+        public string IsyeriSifresi { get; set; }
+        public string KullaniciAdi { get; set; }
+        public SgkService(string url, string kullaniciAdi, string isyeriKodu, string isyeriSifresi)
+        {
+            endpointAddress = new EndpointAddress(url);
+
+            basicHttpBinding = new BasicHttpBinding(endpointAddress.Uri.Scheme.ToLower() == "http"
+                ? BasicHttpSecurityMode.None
+                : BasicHttpSecurityMode.Transport);
+            basicHttpBinding.OpenTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.CloseTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.ReceiveTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.SendTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.MaxReceivedMessageSize = int.MaxValue;
+            _service = new ViziteGonderService.ViziteGonderClient(basicHttpBinding, endpointAddress);
+            KullaniciAdi = kullaniciAdi;
+            IsyeriKodu = isyeriKodu;
+            IsyeriSifresi = isyeriSifresi;
+        }
+
+        public async Task<ActionResponse<string>> LogiAsyncn() 
+        {
+
+            var response = ActionResponse<string>.Success(200);
+
+            var result = await _service.wsLoginAsync(new ViziteGonderService.wsLoginRequest
+            {
+                isyeriKodu = IsyeriKodu,
+                isyeriSifresi = IsyeriSifresi,
+                kullaniciAdi = KullaniciAdi
+            });
+            if (result.wsLoginReturn.sonucKod !=0)
+            {
+                response.Message = result.wsLoginReturn.sonucAciklama;
+                response.ResponseType = ResponseType.Error;
+                return response;
+               
+            }
+            Token = response.Data = result.wsLoginReturn.wsToken;
+            return response;
+        }
+
+     
+
+
+        public async Task<ActionResponse<string>> IsverenIletisimBilgileriGoruntuAsync()
+        {
+            var response = await LogiAsyncn();
+            if (response.ResponseType == ResponseType.Error)
+            {
+                return response;
+            }
+
+            var result = await _service.isverenIletisimBilgileriGoruntuAsync(new ViziteGonderService.isverenIletisimBilgileriGoruntuRequest
+            {
+                wsToken = Token,
+                isyeriKodu = IsyeriKodu,
+                kullaniciAdi = KullaniciAdi
+            });
+
+            if (result.isverenIletisimBilgileriGoruntuReturn.sonucKod != 0)
+            {
+                response.Message = result.isverenIletisimBilgileriGoruntuReturn.sonucAciklama;
+                response.ResponseType = ResponseType.Error;
+            }
+           
+            return response;
+        }
+
+        public async Task<ActionResponse<string>> RaporAramaKimlikNoAsync(string tckNo)
+        {
+            var response = await LogiAsyncn();
+            if (response.ResponseType == ResponseType.Error)
+            {
+                return response;
+            }
+            var result = await _service.raporAramaKimlikNoAsync(new ViziteGonderService.raporAramaKimlikNoRequest
+            {
+                isyeriKodu = IsyeriKodu,
+                kullaniciAdi = KullaniciAdi,
+                wsToken = Token,
+                tckNo = tckNo
+            });
+            if (result.raporAramaKimlikNoReturn.sonucKod != 0)
+            {
+                response.Message = result.raporAramaKimlikNoReturn.sonucAciklama;
+                response.ResponseType= ResponseType.Error;
+                return response;
+            }
+           
+            return response;
+        }
+
+
+    }
+}
